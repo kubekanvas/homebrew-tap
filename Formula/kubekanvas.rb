286abc7fd011a4178a47e5230d75e50e21922e49
Shamaila Mahmood <shamaila@indusvalleylabs.com>
2025-08-27 14:10:37 +0200

    Fixed typo in the url

--- Formula/kubekanvas.rb
+++ Formula/kubekanvas.rb
@@ -1,7 +1,7 @@
 class Kubekanvas < Formula
   desc "KubeKanvas CLI"
   homepage "https://kubekanvas.io"
-  url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/x64/9349862/kubekanva", :using => :curl
+  url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/x64/9349862/kubekanvas", :using => :curl
   sha256 "9555835556b9bd9961df1924b1337b951a7846d4dfc3861723fcde219035ab06"
   version "1.0.0"
 

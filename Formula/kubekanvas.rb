df24dd82c386efd00d3630e4950cf9f4253499c4
Shamaila Mahmood <shamaila@indusvalleylabs.com>
2026-01-18 18:19:18 +0100

    Update kubekanvas formula URLs and SHA256 checksums for version 1.0.1

--- Formula/kubekanvas.rb
+++ Formula/kubekanvas.rb
@@ -2,13 +2,13 @@ class Kubekanvas < Formula
   desc "KubeKanvas CLI"
   homepage "https://www.kubekanvas.io"
   if Hardware::CPU.arm?
-    url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/arm64/9349862/kubekanvas", :using => :curl
-    sha256 "c02a99918277ebde0e2d86136360cb40f5fc5e82383c7182b73592efb980bc66"
+    url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/arm64/1c3d9d5/kubekanvas", :using => :curl
+    sha256 "3495af422fd8565e56a856f7faa9d89b84c0eb89ddce198de0da1708a761cc11"
   else
-    url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/x64/9349862/kubekanvas", :using => :curl
-    sha256 "9555835556b9bd9961df1924b1337b951a7846d4dfc3861723fcde219035ab06"
+    url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/x64/1c3d9d5/kubekanvas-dmg", :using => :curl
+    sha256 "1d32400c414fb2415d6635dfceb9e5a9c7bd6151dbd3886d0289f55790cf7954"
   end
-  version "1.0.0"
+  version "1.0.1"
 
   def install
   downloaded = Dir["*"].find { |f| f.include?("kubekanvas") }

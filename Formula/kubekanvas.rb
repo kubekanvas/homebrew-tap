6e1978888340ab1590d410c57f54dd0f962a9b2c
Shamaila Mahmood <shamaila@indusvalleylabs.com>
2026-02-05 22:35:00 +0100

    Update kubekanvas formula description, URLs, and SHA256 checksums for ARM and x64 architectures

--- Formula/kubekanvas.rb
+++ Formula/kubekanvas.rb
@@ -1,18 +1,19 @@
 class Kubekanvas < Formula
-  desc "KubeKanvas CLI"
+  desc "KubeKanvas CLI - Manage your Kubernetes clusters visually"
   homepage "https://www.kubekanvas.io"
+  version "1.0.0"
+  license :cannot_represent
+
   if Hardware::CPU.arm?
-    url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/arm64/1c3d9d5/kubekanvas", :using => :curl
-    sha256 "3495af422fd8565e56a856f7faa9d89b84c0eb89ddce198de0da1708a761cc11"
+    url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/arm64/latest/kubekanvas"
+    sha256 "d5daa8c58dc2c30c6096274eecedcd1ba60b68e8c13dee8bbaab8f24b846fafd"
   else
-    url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/x64/1c3d9d5/kubekanvas-dmg", :using => :curl
-    sha256 "1d32400c414fb2415d6635dfceb9e5a9c7bd6151dbd3886d0289f55790cf7954"
+    url "https://s3.us-east-1.amazonaws.com/downloads.kubekanvas.io/macos/x64/latest/kubekanvas"
+    sha256 "8b04868ed9a9847e8c8127f10e6dad48a2da8f3710804f9c71b65a3b4c05c719"
   end
-  version "1.0.1"
 
   def install
-  downloaded = Dir["*"].find { |f| f.include?("kubekanvas") }
-  bin.install downloaded => "kubekanvas"
+    bin.install "kubekanvas"
   end
 
   test do
